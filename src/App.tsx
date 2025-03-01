0c2dd5afe8047e1df574fb550a973d7d17b94e56
hossainarif37 <hossainarif4937@gmail.com>
2025-03-01 16:06:17 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      App
+    </div>
+  );
+};
+
+export default App;
